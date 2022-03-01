796dc425bec3fc196dd3946c0f3f4a2dea0fe9a7
George Pastushok <gpastushok@yahoo.com>
2022-02-28 22:59:29 -0800

    added requires to the server.js file

--- server.js
+++ server.js
@@ -1,20 +1,26 @@
-const express = require('express')
-const app = express()
-const port = 3000
-
+// requires for external dependencies
+// express:
+const express = require('express');
+const app = express();
+const port = 3000;
+// inquirer:
 const inquirer = req('inquirer');
+// fs
+const fs = require('fs');
 
+// in-app libraries:
+const Employees = req('./lib/employees');
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.get('/', (req, res) => res.send('Hello World!'));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 
 // post the recieved data
 app.post('/', function (req, res) {
-  res.send('POST request to the homepage')
+  res.send('POST request to the homepage');
 })
 
 // get the data
 app.get('/', (req, res) => {
-  res.send('GET request to the homepage')
+  res.send('GET request to the homepage');
 })
