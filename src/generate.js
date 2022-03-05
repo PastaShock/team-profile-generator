28aa297c8563deeb5c1e47381448e261cfd2030c
George Pastushok <gpastushok@yahoo.com>
2022-03-05 01:50:02 -0800

    added new tests

--- src/generate.js
+++ src/generate.js
@@ -1,6 +1,6 @@
 const newLocal = '../lib/EmployeeTypes';
 const { Engineer, Intern, Manager } = require(newLocal);
-import sources from '../lib/sources';
+const sources = require('../lib/sources');
 
 
 function generateCards(team) {
@@ -10,69 +10,36 @@ function generateCards(team) {
     switch(teamArray.getRole()) {
       case 'Manager':
         const manager = new Manager(teamArray.id, teamArray.name, teamArray.email, teamArray.officeNumber);
-        cards.push(generateManagerCard(manager));
+        auxKey = "office number";
+        cards.push(generateCard(manager.name, manager.role, manager.id, manager.email, auxkey, manager.officenumber));
         break;
       case 'Engineer':
         const engineer = new Engineer(teamArray.id, teamArray.name, teamArray.email, teamArray.github);
-        cards.push(generateEngineerCard(engineer));
+        auxKey = "github";
+        cards.push(generateCard(engineer.name, engineer.role, engineer.id, engineer.email, auxkey, engineer.officenumber));
         break;
       case 'Intern':
         const intern = new Intern(teamArray.id, teamArray.name, teamArray.email, teamArray.school);
-        cards.push(generateInternCard(intern));
+        auxKey = "school";
+        cards.push(generateCard(intern.name, intern.role, intern.id, intern.email, auxkey, intern.officenumber));
         break;
     }
   }
   return cards.join(``)
 }
 
-let generateManagerCard = (Manager) => {
+let generateCard = (name, role, id, email, auxKey, auxVal) => {
   return `
   <div class="card m-1 shadow" style="width: 18rem">
     <div class='card-header'>
-      <h3 class="card-title">${Manager.getName()}</h3>
-      <h6 class="card-text"><i class="fa fa-coffee"></i> ${Manager.getRole()}</h6>
+      <h3 class="card-title">${name}</h3>
+      <h6 class="card-text"><i class="fa fa-coffee"></i> ${role}</h6>
     </div>
     <div class="card-body">
       <ul class="list-group list-group-flush">
-        <li class="list-group-item">ID: ${Manager.getId()}</li>
-        <li class="list-group-item">Email: ${Manager.getEmail()}</li>
-        <li class="list-group-item">Office Number: ${Manager.getOfficeNumber()}</li>
-      </ul>
-    </div>
-  </div>
-  `
-}
-
-let generateEngineerCard = (Engineer) => {
-  return `
-  <div class="card m-1 shadow" style="width: 18rem">
-    <div class='card-header'>
-      <h3 class="card-title">${Engineer.getName()}</h3>
-      <h6 class="card-text"><i class="fa fa-laptop"></i> ${Engineer.getRole()}</h6>
-    </div>
-    <div class="card-body">
-      <ul class="list-group list-group-flush">
-        <li class="list-group-item">ID: ${Engineer.getId()}</li>
-        <li class="list-group-item">Email: ${Engineer.getEmail()}</li>
-        <li class="list-group-item">GitHub: ${Engineer.getGithub()}</li>
-      </ul>
-    </div>
-  </div>
-  `
-};
-
-let generateInternCard = (Intern) => {
-  return `
-  <div class="card m-1 shadow" style="width: 18rem">
-    <div class='card-header'>
-      <h3 class="card-title">${Intern.getName()}</h3>
-      <h6 class="card-text"><i class="fa fa-graduation-cap"></i> ${Intern.getRole()}</h6>
-    </div>
-    <div class="card-body">
-      <ul class="list-group list-group-flush">
-        <li class="list-group-item">ID: ${Intern.getId()}</li>
-        <li class="list-group-item">Email: ${Intern.getEmail()}</li>
-        <li class="list-group-item">School: ${Intern.getSchool()}</li>
+        <li class="list-group-item">ID: ${id}</li>
+        <li class="list-group-item">Email: ${email}</li>
+        <li class="list-group-item">${auxKey}: ${auxVal}</li>
       </ul>
     </div>
   </div>
@@ -88,7 +55,7 @@ return `
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  ${sources.fontAwesome}
   ${sources.bootstrap}
   <title>My Team</title>
 </head>
